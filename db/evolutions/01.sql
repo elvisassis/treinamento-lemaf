86c0770985613ee4da344082dcbc48713ee71c0d
Elvis <elvis.assis.lemaf@gmail.comclear>
2017-10-09 15:34:10 -0300

    Criação da evolution 01.sql para inclusão de operadoras no banco

---
+++ db/evolutions/01.sql
@@ -0,0 +1,8 @@
+# --- !Ups
+
+
+INSERT INTO operadora (nome, codigo, categoria, preco) VALUES ('Tim', 41, 'Celular', 2);
+INSERT INTO operadora (nome, codigo, categoria, preco) VALUES ('Vivo', 14, 'Celular', 1);
+INSERT INTO operadora (nome, codigo, categoria, preco) VALUES ('Oi', 16, 'Celular', 3);
+INSERT INTO operadora (nome, codigo, categoria, preco) VALUES ('GVT', 13, 'Fixo', 2);
+INSERT INTO operadora (nome, codigo, categoria, preco) VALUES ('Embratel', 14, 'Fixo',1);
